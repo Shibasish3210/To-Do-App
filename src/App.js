d430f63098a58d35cdd694d2441b4478decdd784
unknown <shibasish3210@gmail.com>
2024-01-19 21:49:06 +0530

    updated codebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,80 @@
-import React from 'react';
-import Header from './components/Header';
+import React, {useState,useEffect} from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Task from './components/Task';
 
-
+const LOCAL_SORAGE_KEY = 'TASKS';
+let input1, input2;
+let prevData;
 const App=()=>{
+    const [taskDetails, setTaskDetails] = useState([]);
+    const[currTask, setCurrTask] = useState({
+        title: '',
+        desc : '',
+        id : '',
+        checked: false,
+        expanded : false
+    });
+    
+    
+    prevData = JSON.parse(localStorage.getItem(LOCAL_SORAGE_KEY));
+    useEffect(()=>{
+        if(prevData){
+            setTaskDetails(prevData);
+        }
+        
+    },[])
+    function addCurrTask() {
+        if(!input1 || !input2 || input1.value === '' || input2.value === ''){
+            alert("Please Enter Both Fields");
+            return;
+        }
+
+        for (let i = 0; i < taskDetails.length; i++) {
+            if(taskDetails[i].id === currTask.id){
+                return;
+            }
+            
+        }
+        
+        setTaskDetails([...taskDetails,currTask]);
+        input1.value = '';
+        input2.value = '';
+        
+    }
+    localStorage.setItem(LOCAL_SORAGE_KEY,JSON.stringify([...taskDetails]));
+
+    function addTaskTitle(e) {
+        e.target.value && setCurrTask({...currTask,title:e.target.value,id:uuidv4()});
+        input1 = e.target;
+    }
+    
+    function addTaskDesc(e) {
+        e.target.value && setCurrTask({...currTask,desc:e.target.value});
+        input2 = e.target;
+    }
+
     return (
         <>
-        <Header/>
+        <div className='Header'>
+            <h1 className='heading'>To-Do App</h1>
+            <div className="inp-fields">
+                <label htmlFor="task-heading">Task Heading : </label>
+                <input type="text" id='task-heading' placeholder='Enter Task Heading' onBlur={e=>addTaskTitle(e)} />
+            </div>
+            <div className="inp-fields">
+                <label htmlFor="task-description">Task Description : </label>
+                <input type="text" id='task-description' placeholder='Enter Task Details' onBlur={e=>addTaskDesc(e)} />
+            </div>
+            <button className='btn' onClick={addCurrTask}>Add Task</button>
+        </div>
+
         <div className="task-container">
-            <Task title={"Do Eat"}/>
+            {
+                taskDetails.length >=1 &&
+                taskDetails.map((tasks)=>{
+                    return <Task key={tasks.id} title={tasks.title} description={tasks.desc} taskDetails={taskDetails} setTaskDetails={setTaskDetails} id={tasks.id}/>
+                })
+            }
         </div>
         </>
     );    
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,47 @@
-import React from 'react';
-import KnowMore from "../assets/know.svg";
-import Delete from "../assets/delete.svg";
+import React,{useState} from 'react';
+// import KnowMore from "../assets/know.svg";
+// import Delete from "../assets/delete.svg";
 
-const Task = ({title,})=>{
+const Task = ({title,description,taskDetails,setTaskDetails,id})=>{
+    const [completed, setCompleted] = useState(false);
+    const [expanded, setExpanded] = useState(false);
+
+    function toggleCass(e) {
+        if(e.target.checked){
+            setCompleted(true);
+        }else{
+            setCompleted(false);
+        }
+    }
+
+    function deleteTask() {
+        setTaskDetails(taskDetails.filter((task)=>{
+            return task.id !== id;
+        }))
+    }
+
+    function handleExpansion() {
+        console.log("click");
+        if(expanded){
+            setExpanded(false);
+        }else{
+            setExpanded(true);
+        }
+    }
+
     return (
+    <div className={completed ? 'Task-wrapper done' : 'Task-wrapper'}>
     <div className='Task-card'>
-        <input type="checkbox" className="status" />
+        <input className='status' type="checkbox" onClick={e=>toggleCass(e)}/>
         <h3 className="task-title">{title}</h3>
-        <KnowMore/>
-        <Delete/>
+        <img onClick={handleExpansion} width="18" height="18" src="https://img.icons8.com/windows/32/info.png" alt="info"/>
+        <img onClick={deleteTask} className='delete' width="35" height="35" src="https://img.icons8.com/carbon-copy/100/filled-trash.png" alt="filled-trash"/>
+    </div>
+    <div className="extra">
+        <p className={expanded ? 'noClass' : 'extra-info'}>
+            {description}
+        </p>
+    </div>
     </div>
     );
 }
